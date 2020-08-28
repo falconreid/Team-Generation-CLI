c89b5aa74b7d25ea8976a01c09197b8c7730d742
Joel Terrell <falconreid@hotmail.com>
2020-08-28 16:13:29 -0600

    reconfigured code for manager setup first, then team members

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+var validator = require("email-validator");
+
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -10,6 +12,147 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+function createManager() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Please enter the manager's name.",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Please enter the manager's ID.",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Please enter the manager's email.",
+        validate: (answer) => {
+          const emailCheck = answer.match(/\S+@\S+\.\S+/);
+          if (emailCheck) {
+            return true;
+          }
+          return "Please enter a valid email address.";
+        },
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "Please enter the manager's phone number.",
+      },
+    ])
+    .then(function (answers) {
+      const manager = new manager(
+        answers.name,
+        answers.id,
+        answers.email,
+        answers.officeNumber
+      );
+    });
+  teamPrompt();
+}
+
+function teamPrompt() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "role",
+        message: "What type of employee would you like to add?",
+        choices: ["Engineer", "Intern", "No More Employees"],
+      },
+    ])
+    .then((answers) => {
+      switch (answers.role) {
+        case "Engineer":
+          inquirer
+            .prompt([
+              {
+                type: "input",
+                name: "name",
+                message: "What is the engineers's name?",
+              },
+              {
+                type: "input",
+                name: "id",
+                message: "What is the engineers's ID?",
+              },
+              {
+                type: "input",
+                name: "email",
+                message: "What is the engineers's email?",
+                validate: (answer) => {
+                  const emailCheck = answer.match(/\S+@\S+\.\S+/);
+                  if (emailCheck) {
+                    return true;
+                  }
+                  return "Please enter a valid email address.";
+                },
+              },
+              {
+                type: "input",
+                name: "github",
+                message: "What is the engineers's Github username?",
+              },
+            ])
+            .then(function (answers) {
+              const engineer = new engineer(
+                answers.name,
+                answers.id,
+                answers.email,
+                answers.github
+              );
+            });
+          break;
+        case "Intern":
+          inquirer
+            .prompt([
+              {
+                type: "input",
+                name: "name",
+                message: "What is the intern's name?",
+              },
+              {
+                type: "input",
+                name: "id",
+                message: "What is the intern's ID?",
+              },
+              {
+                type: "input",
+                name: "email",
+                message: "What is the intern's email?",
+                validate: (answer) => {
+                  const emailCheck = answer.match(/\S+@\S+\.\S+/);
+                  if (emailCheck) {
+                    return true;
+                  }
+                  return "Please enter a valid email address.";
+                },
+              },
+              {
+                type: "input",
+                name: "school",
+                message: "What school is the intern attending?",
+              },
+            ])
+            .then(function (answers) {
+              const intern = new intern(
+                answers.name,
+                answers.id,
+                answers.email,
+                answers.school
+              );
+            });
+          break;
+        case "No More Employees":
+          console.log("Employee profiles are done!");
+      }
+    });
+}
+
+createManager();
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
